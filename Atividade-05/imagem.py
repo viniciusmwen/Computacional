fae8923883f3ba336c0646d7022f8a0c35c6f971
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-01-19 16:08:24 -0300

    Código para funções padrões de imagens

---
+++ Atividade-05/imagem.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from skimage.io import imread,imsave,imshow
+from skimage.color import rgb2gray
+import cv2
+import os
+import csv
+from tqdm import tqdm
+from skimage.transform import resize
+
+class Imagem():
+
+    def __init__(self, name='sunflower.jpg'):
+        self.name = name
+        self.img = None
+        self.imgGray = None
+        self.imgRed = None
+        self.altura = 240
+        self.largura = 240
+        self.dimensoes = None
+    
+    # Ler a imagem, imagem redimensionada e imagem em tons de cinza.
+    def ler(self):
+        self.img = imread(self.name)
+        self.imgRed = resize(self.img, (self.largura, self.altura))
+        self.imgGray = rgb2gray(self.imgRed)
+
+    def shows(self, qnt=2):
+
+        if qnt == 0:
+            self.show(self.img)
+
+        elif qnt == 1:
+            self.manyShowWithTitle(
+                {'img': self.img, 'title': f'Tamanho original'},
+                {'img': self.imgRed, 'title': f'Tamanho padronizado'},
+                color='gray')
+
+        elif qnt == 2:
+            self.manyShowWithTitle(
+                {'img': self.img, 'title': f'Tamanho original'},
+                {'img': self.imgRed, 'title': f'Tamanho padronizado'},
+                {'img': self.imgGray, 'title': f'Preto e Branco'},
+                color='gray')
+    
+    def show(self, img=None):
+        if img is None:
+            img = self.img
+        plt.imshow(img, cmap='gray')
+        plt.title(f'{img.shape}')
+        plt.axis('off')
+
+    # Função que apresenta várias imagens.
+    def manyShow(self, *imgs, color=None):
+        _, ax = plt.subplots(1, len(imgs), figsize=(20, 20),sharex=True)
+        for index, i in enumerate(ax):
+            i.imshow(imgs[index], cmap=color)
+            i.set_title(f'{imgs[index].shape}')
+            i.axis('off')
+
+    # Função que apresenta várias imagens com títulos.
+    def manyShowWithTitle(self, *imgs, color=None):
+        _, ax = plt.subplots(1, len(imgs), figsize=(20, 20),sharex=True)
+        for index, i in enumerate(ax):
+            i.imshow(imgs[index]['img'], cmap=color)
+            i.set_title(f'{imgs[index]["title"]} \n {imgs[index]["img"].shape}')
+            i.axis('off')
+
+
+
+
